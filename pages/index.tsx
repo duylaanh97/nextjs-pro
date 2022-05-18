7a81d4ed1e04b1ab5aba5bb18842b2c5d3eb0d1c
duylaanh97 <leezy.pz66@gmail.com>
2022-05-18 21:46:43 +0700

    seting up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
+  const goToDetailPage = () => {
+    router.push({
+      pathname: '/posts/[postId]',
+      query: {
+        postId: 123,
+        ref: 'leezy',
+      },
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,6 +33,12 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
+
+        <Link href="/about">
+          <a>Go to About</a>
+        </Link>
+
+        <button onClick={goToDetailPage}>Go to Post Detail Page</button>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -53,10 +72,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by Leezy
         </a>
       </footer>
     </div>
---
+++ pages/posts/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export interface PostlistProps {}
+
+export default function Postlist(props: PostlistProps) {
+  return <div>Postlist Page</div>
+}
